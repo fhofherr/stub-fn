5b6c7c1ef7effcc081f79c50116b47377345ba03
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2017-04-23 13:56:13 +0200

    First stab at stub-fn macro.

--- src/fhofherr/stub_fn/core.clj
+++ src/fhofherr/stub_fn/core.clj
@@ -17,3 +17,37 @@
                                 (recur ys found-syms))
               :else found-syms))]
     (search-syms (seq fn-params) [])))
+
+
+(defmacro stub-fn
+  [fn-params & fn-body]
+  (let [param-syms (#'find-fn-param-syms fn-params)]
+    `(let [stub-invocations# (ref [])
+           stubbed-fn# (fn [~@fn-params]
+                         (let [invocation-args# (into {}
+                                                      [~@(for [s param-syms]
+                                                           `['~s ~s])])
+                               return-value# (do ~@fn-body)
+                               invocation# {::invocation-args invocation-args#
+                                            ::return-value return-value#}]
+                           (dosync
+                             (alter stub-invocations# conj invocation#))
+                           return-value#))]
+       (vary-meta stubbed-fn# assoc ::stub-fn stub-invocations#))))
+
+(defn stub-info
+  [stubbed-fn]
+  @(-> stubbed-fn meta ::stub-fn))
+
+
+(defn- filter-by-args
+  [stub-infos expected-args]
+  (filter #(-> % ::invocation-args (= expected-args)) stub-infos))
+
+
+(defn invoked?
+  [stubbed-fn & {:keys [times args] :or {times 1}}]
+  (let [invocations (as-> stubbed-fn $
+                      (stub-info $)
+                      (if args (filter-by-args $ args) $))]
+    (-> invocations count (= times))))
--- test/fhofherr/stub_fn/test/core_test.clj
+++ test/fhofherr/stub_fn/test/core_test.clj
@@ -18,3 +18,59 @@
     (is (= '[a b] (#'core/find-fn-param-syms '[{a :a b :b}])))
     (is (= '[a b] (#'core/find-fn-param-syms '[{:keys [a b]}])))
     (is (= '[a b] (#'core/find-fn-param-syms '[{:keys [a b] :or {a c b d}}])))))
+
+
+(deftest stubbing-functions
+
+  (testing "returns nil if no body is given"
+    (let [f (core/stub-fn [])
+          result (f)]
+      (is (nil? result))))
+
+  (testing "all args are available within the function"
+
+    (let [f (core/stub-fn [a b c] (+ a b c))]
+      (is (= (+ 1 2 3) (f 1 2 3))))
+
+    (let [f (core/stub-fn [a b c]
+                          (+ a b c)
+                          (- a b c))]
+      (is (= (- 1 2 3) (f 1 2 3))))))
+
+
+(deftest checking-function-invocation
+
+  (testing "all invocations are registered"
+    (let [f (core/stub-fn [])]
+      (f)
+      (is (core/invoked? f)))
+
+    (let [never-invoked (core/stub-fn [])]
+      (is (not (core/invoked? never-invoked)))))
+
+  (testing "the number of expected invocatios can be specified"
+    (let [f (core/stub-fn [])]
+      (f)
+      (is (core/invoked? f :times 1))
+      (is (not (core/invoked? f :times 2)))
+      (f)
+      (is (core/invoked? f :times 2))))
+
+  (testing "the expected arguments can be specified"
+    (let [f (core/stub-fn [x y])]
+      (f 1 2)
+      (is (core/invoked? f :args {'x 1 'y 2}))
+      (is (not (core/invoked? f :args {'x 3 'y 4})))
+      (is (not (core/invoked? f :args {'a 3 'b 4})))))
+
+  (testing "invocation counts relate to the expected arguments"
+    (let [f (core/stub-fn [x y])]
+      (f 1 2)
+      (is (core/invoked? f :args {'x 1 'y 2} :times 1))
+      (is (not (core/invoked? f :args {'x 1 'y 2} :times 2)))
+      (f 1 2)
+      (is (core/invoked? f :args {'x 1 'y 2} :times 2))
+      (f 3 4)
+      (is (core/invoked? f :args {'x 1 'y 2} :times 2))
+      (is (core/invoked? f :args {'x 3 'y 4} :times 1))
+      (is (not (core/invoked? f :args {'x 3 'y 4} :times 2))))))
