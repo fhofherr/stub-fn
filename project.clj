21adee9a72c531012d0de30d70a58e0d4035afa6
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2017-04-23 10:55:22 +0200

    Initial import

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject fhofherr/stub-fn "0.1.0-SNAPSHOT"
+  :description "A macro to stub arbitrary clojure functions."
+  :url "https://github.com/fhofherr/stub-fn"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
