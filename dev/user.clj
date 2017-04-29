6c4dc50464f41959fec45997512b09e965249a34
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2017-04-29 12:13:43 +0200

    Add run-tests function.

--- dev/user.clj
+++ dev/user.clj
@@ -3,5 +3,14 @@
             [clojure.string :as str]
             [clojure.pprint :refer (pprint)]
             [clojure.repl :refer :all]
-            [clojure.test :as test]
+            [clojure.test :as t]
             [clojure.tools.namespace.repl :refer (refresh refresh-all)]))
+
+
+(defn- do-test
+  []
+  (t/run-all-tests #"fhofherr\.stub-fn\.test\..+-test"))
+
+(defn run-tests
+  []
+  (refresh :after 'user/do-test))
